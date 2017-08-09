c5d5d5740c37001819391413761cdcb650506dca
BME319 <zjubme319@163.com>
2017-08-09 22:16:01 +0800

    返回历史控制
    Signed-off-by: BME319 <zjubme319@163.com>

--- www/js/app.js
+++ www/js/app.js
@@ -95,10 +95,22 @@ angular.module('kidney', [
               var errs = []
 
               if (state == 'testqrcode' || state == 'qrcode') {
+                $ionicHistory.nextViewOptions({
+                  disableBack: true,
+                  disableAnimate: true
+                })
                 $state.go('myqrcode')
               } else if (state == 'testnewsufferer' || state == 'newsufferer') {
+                $ionicHistory.nextViewOptions({
+                  disableBack: true,
+                  disableAnimate: true
+                })
                 $state.go('tab.patient')
               } else if (params.length > 1 && params[0] == 'doctor') {
+                $ionicHistory.nextViewOptions({
+                  disableBack: true,
+                  disableAnimate: true
+                })
                 if (params[1] == '13') { $state.go('tab.group-chat', {type: params[2], groupId: params[3], teamId: params[4]}) } else { $state.go('tab.detail', {type: params[2], chatId: params[3], counselId: params[4]}) }
               } else {
                 $q.all([
